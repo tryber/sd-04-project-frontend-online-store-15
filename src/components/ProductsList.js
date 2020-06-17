9b8a3fbaa8bb0769e6a9a4f1c33e4c4cbff9fd71
Caio Magalhães <caio.lmf@gmail.com>
2020-06-17 18:26:36 -0300

    Add ProductList complete component

---
+++ src/components/ProductsList.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import * as api from '../services/api';
+
+export class ProductList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: props.query,
+      categorieId: prop.categorieId,
+      products: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ products: this.getProducts() });
+  }
+
+  getProducts() {
+    const { query, categorieId } = this.state;
+    let products = [];
+
+    if (query && !categorieId) {
+      products = api.getProductsFromCategoryAndQuery(null, query);
+    } else if (!query && categorieId) {
+      products = api.getProductsFromCategoryAndQuery(categorieId, null);
+    } else {
+      products = api.getProductsFromCategoryAndQuery(categorieId, query);
+    }
+
+    return products;
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <div>
+        {products.map((product) => {
+          return (
+            <div data-testid="product" className="product-card">
+              <div className="product-title">{product.title}</div>
+              <div className="product-info">
+                <img src={product.thumbnail} alt="product" />
+                <p>{`R$ ${product.price}`}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default ProductList;
