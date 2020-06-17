a03bdea6af9a2aa373ab49e10f209fb03f165a2e
Caio Magalhães <caio.lmf@gmail.com>
2020-06-17 16:57:05 -0300

    Define searchText state

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,12 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export class ProductList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { searchText: '' };
+  }
+
   render() {
     return <div />;
   }
