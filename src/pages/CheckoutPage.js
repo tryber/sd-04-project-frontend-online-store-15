fcdfcf08d5da622e3921ff3fce38f338a9fcfffe
Caio Magalhães <caio.lmf@gmail.com>
2020-06-29 02:42:06 -0300

    Changes for CC

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -9,32 +9,13 @@ class CheckoutPage extends Component {
     return (
       <div>
         <form>
-          <input
-            placeholder="Nome completo"
-            data-testid="checkout-fullname"
-            formNoValidate
-          />
-          <input
-            placeholder="Email"
-            data-testid="checkout-email"
-            formNoValidate
-          />
+          <input placeholder="Nome completo" data-testid="checkout-fullname" formNoValidate />
+          <input placeholder="Email" data-testid="checkout-email" formNoValidate />
           <input placeholder="CPF" data-testid="checkout-cpf" formNoValidate />
-          <input
-            placeholder="Telefone"
-            data-testid="checkout-phone"
-            formNoValidate
-          />
+          <input placeholder="Telefone" data-testid="checkout-phone" formNoValidate />
           <input placeholder="CEP" data-testid="checkout-cep" formNoValidate />
-          <input
-            placeholder="Endereço"
-            data-testid="checkout-address"
-            formNoValidate
-          />
-          <button
-            type="button"
-            onClick={() => this.window.alert('Compra Finalizada')}
-          >
+          <input placeholder="Endereço" data-testid="checkout-address" formNoValidate />
+          <button type="button" onClick={() => this.window.alert('Compra Finalizada')}>
             Finalizar compra
           </button>
         </form>
@@ -43,7 +24,6 @@ class CheckoutPage extends Component {
   }
 
   render() {
-    const { location: { list } } = this.props;
     return (
       <div>
         <h1>Revise seus produtos</h1>
@@ -57,8 +37,8 @@ class CheckoutPage extends Component {
           Total:
           {Math.round(
             (this.getCart().cartList.reduce(
-              (total, product) => total + product.price * product.selected_quantity, 0
-            )) * 100
+              (total, product) => (total + (product.price * product.selected_quantity)), 0,
+            )) * 100,
           ) / 100}
         </h1>
         {this.checkoutForm()}
