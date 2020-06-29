b076cf9338004834e930a6ae1dd5ea3646b03857
Caio Magalhães <caio.lmf@gmail.com>
2020-06-29 02:16:17 -0300

    Load cart from local storage on chackout page for products check

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 
 class CheckoutPage extends Component {
+  getCart() {
+    return JSON.parse(localStorage.getItem('cartState'));
+  }
+
   checkoutForm() {
     return (
       <div>
         <form>
-          <input placeholder="Nome completo" data-testid="checkout-fullname" formNoValidate />
-          <input placeholder="Email" data-testid="checkout-email" formNoValidate />
+          <input
+            placeholder="Nome completo"
+            data-testid="checkout-fullname"
+            formNoValidate
+          />
+          <input
+            placeholder="Email"
+            data-testid="checkout-email"
+            formNoValidate
+          />
           <input placeholder="CPF" data-testid="checkout-cpf" formNoValidate />
-          <input placeholder="Telefone" data-testid="checkout-phone" formNoValidate />
+          <input
+            placeholder="Telefone"
+            data-testid="checkout-phone"
+            formNoValidate
+          />
           <input placeholder="CEP" data-testid="checkout-cep" formNoValidate />
-          <input placeholder="Endereço" data-testid="checkout-address" formNoValidate />
-          <button type="button" onClick={() => this.window.alert('Compra Finalizada')}>
+          <input
+            placeholder="Endereço"
+            data-testid="checkout-address"
+            formNoValidate
+          />
+          <button
+            type="button"
+            onClick={() => this.window.alert('Compra Finalizada')}
+          >
             Finalizar compra
           </button>
         </form>
@@ -32,7 +55,11 @@ class CheckoutPage extends Component {
         ))}
         <h1>
           Total:
-          {Math.round(list.map((inicial) => inicial.price).reduce((initial, acc) => initial + acc))}
+          {Math.round(
+            (this.getCart().cartList.reduce(
+              (total, product) => total + product.price * product.selected_quantity, 0
+            )) * 100
+          ) / 100}
         </h1>
         {this.checkoutForm()}
       </div>
